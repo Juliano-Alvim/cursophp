6e8c73e746de09d19c031865bf112df1152036ae
Juliano-Alvim <julianoalvim08@gmail.com>
2026-07-06 12:00:39 -0300

    Primeira aula PHP

--- ex001/index.php
+++ ex001/index.php
@@ -3,11 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Meu primeiro teste PHP</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Dados do Servidor</title>
 </head>
 <body>
-    <h1>Testando PHP</h1>
-    <P>Vou aprender PHP agora</P>
+    <h1>Dados do Servidor</h1>
+    <?php
+    phpinfo();
+    ?>
 </body>
 </html>
